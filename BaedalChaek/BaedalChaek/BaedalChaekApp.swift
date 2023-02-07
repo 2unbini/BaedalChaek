dadfec3ff2196fc7325e7192a25fc9a49ae434a7
Eunbin Kwon <kwoneunbini@gmail.com>
2023-02-07 17:16:35 +0900

    Initial Project

---
+++ BaedalChaek/BaedalChaek/BaedalChaekApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaedalChaekApp.swift
+//  BaedalChaek
+//
+//  Created by 권은빈 on 2023/02/07.
+//
+
+import SwiftUI
+
+@main
+struct BaedalChaekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
